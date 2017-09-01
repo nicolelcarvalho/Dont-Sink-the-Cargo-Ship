6a8abc253ac47421a36c064f244d72cc2eb7fb38
Nicole Carvalho <NicoleCarvalho@NicoleCalhosMBP.home>
2017-09-01 08:38:37 -0400

    Created files and pseudocoded the javascript

---
+++ javascript/game.js
@@ -0,0 +1,28 @@
+// Global Variables 
+	// Store the displayed random number in a variable. The random number should be between 19 - 120.
+	// Store each of the four crystals' random numbers in each of their own variables. Each crystal should have a random hidden value between 1 - 12.
+	// Create a total score variable that adds up each of the four crystals variables.. as an array?
+	// Create a variable for wins. 
+	// Create a variable for losses. 
+
+
+
+// Start the game: The player will be shown a random number at the start of the game.
+
+
+// When the player clicks on a crystal, it will add a specific amount of points to the player's total score.
+// Target the ID's of the elements from the HTML page and create an onclick function. 
+// Your game will hide this amount until the player clicks a crystal.
+// When they do click one, update the player's score counter.
+// Write the total score variable to the HTML div that displays the player's total score. 
+
+
+// Compare the current value of the total score variable with the random number variable. 
+// If the player's score match the random number variable, the player wins and wins increases by 1. 
+// If the players score is greater than the random number, the player loses and losses increases by 1. 
+
+
+// The game restarts whenever the player wins or loses.
+// When the game begins again, the player should see a new random number. 
+// All the crystals will have four new hidden values. 
+// All counters reset to 0 except for wins and losses.
